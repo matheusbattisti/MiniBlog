20de77e6fb847ab9edd04a135cb715b3d87a42f5
Matheus Battisti <matheusbudkewicz@gmail.com>
2017-11-03 08:41:53 -0200

    criando rota para editar usuario

--- core/Route.php
+++ core/Route.php
@@ -14,9 +14,10 @@
 			$routes['adminloginpost'] = array('route'=>'/admin/login/post', 'controller'=>'AdminController', 'action'=>'loginPost');
 			$routes['team'] = array('route'=>'/admin/team', 'controller'=>'AdminController', 'action'=>'listTeam');
 			$routes['adduser'] = array('route'=>'/admin/team/adduser', 'controller'=>'AdminController', 'action'=>'add');
+			$routes['edituser'] = array('route'=>'/admin/team/edituser', 'controller'=>'AdminController', 'action'=>'edit');
 
 			$routes['adduserpost'] = array('route'=>'/user/add/post', 'controller'=>'UserController', 'action'=>'addPost');			
-			$routes['edituser'] = array('route'=>'/user/edituser', 'controller'=>'UserController', 'action'=>'editPost');
+			$routes['edituserpost'] = array('route'=>'/user/edituser', 'controller'=>'UserController', 'action'=>'editPost');
 			$routes['deleteuser'] = array('route'=>'/user/deleteuser', 'controller'=>'UserController', 'action'=>'deletePost');
 			$routes['logout'] = array('route'=>'/user/logout', 'controller'=>'UserController', 'action'=>'logout');
 			$this->setRoutes($routes);
